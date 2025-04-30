422e62ae146e812dcecba4bc60a0a13724248be4
morgan <mc@m-labs.hk>
2025-04-30 16:59:24 +0800

    ploopy classic: update via keymap

--- keyboards/ploopyco/trackball/keymaps/via/keymap.c
+++ keyboards/ploopyco/trackball/keymaps/via/keymap.c
@@ -19,7 +19,13 @@
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT( /* Base */
-        KC_BTN1, DRAG_SCROLL, KC_BTN2,
-          KC_BTN4, KC_BTN5
+        KC_BTN1, KC_BTN3, KC_BTN3,
+          KC_BTN2, DRAG_SCROLL
     ),
 };
+
+#if defined(ENCODER_MAP_ENABLE)
+const uint16_t PROGMEM encoder_map[][NUM_ENCODERS][NUM_DIRECTIONS] = {
+    [0] = { ENCODER_CCW_CW(KC_BTN4, KC_BTN5) },
+};
+#endif
